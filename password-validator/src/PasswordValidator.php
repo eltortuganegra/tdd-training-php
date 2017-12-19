c1f2b8b2d7b550b92e47c89d40684c4c86bb4207
eltortuganegra <jorge@fuss.es>
2017-12-19 08:33:52 +0100

    Password  should has more than eight characters

--- password-validator/src/PasswordValidator.php
+++ password-validator/src/PasswordValidator.php
@@ -5,8 +5,12 @@ namespace PasswordValidator;
 class PasswordValidator
 {
 
-    public function validate()
+    public function validate($password)
     {
+        if (strlen($password) < 8) {
+            return false;
+        }
+
         return true;
     }
 }
--- password-validator/test/PasswordValidatorTest.php
+++ password-validator/test/PasswordValidatorTest.php
@@ -18,9 +18,24 @@ class PasswordValidatorTest extends \PHPUnit_Framework_TestCase
     public function it_should_validate_a_valid_password()
     {
         $passwordValidator = new PasswordValidator();
-
         $validPassword = '12345678Ab1_';
+
         $isValid = $passwordValidator->validate($validPassword);
+
         $this->assertTrue($isValid);
     }
+
+    /** @test */
+    public function it_should_has_a_size_more_than_eight_characters()
+    {
+        $passwordValidator = new PasswordValidator();
+        $validPassword = '1234567';
+
+        $isValid = $passwordValidator->validate($validPassword);
+
+        $this->assertFalse($isValid);
+    }
+
+
+
 }
