f028f29ed0fb28703e571c262e0b122dcccf45b1
eltortuganegra <jorge@fuss.es>
2017-12-20 09:02:21 +0100

    Class should has an update quality method

--- gilded-rose/test/GildedRoseTest.php
+++ gilded-rose/test/GildedRoseTest.php
@@ -10,10 +10,15 @@ class GildedRoseTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function changeMe()
+    public function should_has_update_quality_method()
     {
         $items = array();
         $gildedRose = new GildedRose($items);
-        $this->assertEquals(null, $gildedRose);
+
+        $isUpdateQualityMethodDefined = method_exists($gildedRose, 'update_quality');
+
+        $this->assertTrue($isUpdateQualityMethodDefined);
     }
+
+
 }
