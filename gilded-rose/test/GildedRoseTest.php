2849ee0ee644f24318d9c05a78bfa33258358af4
eltortuganegra <jorge@fuss.es>
2017-12-20 09:22:03 +0100

    Update should decrement sellin value one unit when it update quality

--- gilded-rose/test/GildedRoseTest.php
+++ gilded-rose/test/GildedRoseTest.php
@@ -3,6 +3,7 @@
 namespace GildedRose\Test;
 
 use GildedRose\GildedRose;
+use GildedRose\Item;
 
 class GildedRoseTest extends \PHPUnit_Framework_TestCase
 {
@@ -19,5 +20,22 @@ class GildedRoseTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($isUpdateQualityMethodDefined);
     }
 
+    /**
+     * @test
+     */
+    public function should_decrement_the_sellin_value_one_unit_when_it_update_quality()
+    {
+        $defaultSellinValue = 10;
+        $defaultQualityValue = 10;
+        $standardItem = new Item('standard', $defaultSellinValue, $defaultQualityValue);
+        $items = array($standardItem);
+        $gildedRose = new GildedRose($items);
+
+        $gildedRose->update_quality();
+        $decrementedValue = $defaultSellinValue - 1;
+        $isSellinValueDecremented = ($standardItem->sell_in == $decrementedValue);
+
+        $this->assertTrue($isSellinValueDecremented);
+    }
 
 }
